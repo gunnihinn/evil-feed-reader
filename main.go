6f70a29276cb7d9ed49a133b891671ed4ab24e32
Gunnar Þór Magnússon <gunnar.magnusson@booking.com>
2017-07-12 07:14:43 +0200

    Parse and render a sample feed
    We parse a sample feed from a local file on disk into Go data
    structures, and render the title of the feed and hyperlinks to its
    posts.

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 )
+
+type Feed struct {
+	XMLName xml.Name `xml:"rss"`
+	Title   string   `xml:"channel>title"`
+	Items   []Item   `xml:"channel>item"`
+}
+
+type Item struct {
+	Title       string `xml:"title"`
+	Url         string `xml:"link"`
+	Description string `xml:"description"`
+	Content     string `xml:"content"`
+}
+
+func ParseFeed(feed []byte) (Feed, error) {
+	f := Feed{}
+	err := xml.Unmarshal(feed, &f)
+
+	return f, err
+}
 
 func main() {
 	var port = flag.Int("port", 8080, "HTTP port")
@@ -17,7 +39,20 @@ func main() {
 			fmt.Fprintf(w, "%s", err)
 			return
 		}
-		t.Execute(w, nil)
+
+		test, err := ioutil.ReadFile("example-feed.rss")
+		if err != nil {
+			fmt.Fprintf(w, "%s", err)
+			return
+		}
+
+		feed, err := ParseFeed(test)
+		if err != nil {
+			fmt.Fprintf(w, "%s", err)
+			return
+		}
+
+		t.Execute(w, feed)
 	})
 	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 }
