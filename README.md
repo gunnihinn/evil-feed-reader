6c2d6157bd743e484c9975a4e0df5034db7467cb
Gunnar Þór Magnússon <gunnar.magnusson@booking.com>
2017-07-14 08:35:09 +0200

    Fetch feed from URL
    We still only fetch one hardcoded feed.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # rdr
 
 Reader is an RSS reader.
+
+## TODO
+
+- [x] Fetch feed from URL
+- Read more than one feed
+- Async fetching of feeds
--- main.go
+++ main.go
@@ -12,9 +12,11 @@ import (
 type Feed struct {
 	XMLName xml.Name `xml:"rss"`
 
+	Url string // The URL we get the feed from; may or may not equal Link
+
 	// required
 	Title       string `xml:"channel>title"`
-	Url         string `xml:"channel>link"`
+	Link        string `xml:"channel>link"`
 	Description string `xml:"channel>description"`
 
 	// optional
@@ -29,7 +31,7 @@ type Item struct {
 	 */
 	Title       string        `xml:"title"`
 	Description template.HTML `xml:"description"` // lol security
-	Url         string        `xml:"link"`
+	Link        string        `xml:"link"`
 	Content     template.HTML `xml:"encoded"`
 }
 
@@ -40,9 +42,23 @@ func ParseFeed(feed []byte) (Feed, error) {
 	return f, err
 }
 
+func FetchRawFeed(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	defer resp.Body.Close()
+	blob, err := ioutil.ReadAll(resp.Body)
+
+	return blob, err
+}
+
 func main() {
 	var port = flag.Int("port", 8080, "HTTP port")
 	flag.Parse()
+
+	url := "https://blog.regehr.org/feed"
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		t, err := template.ParseFiles("index.html")
@@ -51,17 +67,18 @@ func main() {
 			return
 		}
 
-		test, err := ioutil.ReadFile("example-feed.rss")
+		blob, err := FetchRawFeed(url)
 		if err != nil {
 			fmt.Fprintf(w, "%s", err)
 			return
 		}
 
-		feed, err := ParseFeed(test)
+		feed, err := ParseFeed(blob)
 		if err != nil {
 			fmt.Fprintf(w, "%s", err)
 			return
 		}
+		feed.Url = url
 
 		t.Execute(w, feed)
 	})
