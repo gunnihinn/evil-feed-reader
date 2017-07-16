2fa0bf72e9758e1a9e542918a5a87973cbed3081
Gunnar Þór Magnússon <gunnar.magnusson@booking.com>
2017-07-16 20:36:23 +0200

    Dynamically generate handlers
    Each feed now has its own handler that shows the items in that feed.
    We will eventually use this to also generate a "latest" feed that mixes
    the N latest items across all feeds.

--- README.md
+++ README.md
@@ -27,10 +27,10 @@ These are the initial release goals.
 - [x] Render more than one feed
 - [x] Async fetching of feeds
 - [x] Guard against feeds including N-thousand word posts in the description.
+- [x] Dynamically generate handlers for each feed
+- Render the N newest items across all feeds separately
 - Don't hardcode feed URLs
 - Make the rendering pretty
-- Dynamically generate handlers for each feed
-- Render the N newest items across all feeds separately
 
 Nice-to-haves (?):
 
--- cmd/evil-rss-reader/main.go
+++ cmd/evil-rss-reader/main.go
@@ -16,6 +16,41 @@ var URLS = []string{
 	"http://ithare.com/feed/",
 }
 
+type Context struct {
+	Sidebar []Navitem
+	Feed    reader.Feed
+}
+
+type Navitem struct {
+	Title    string
+	Resource string
+}
+
+func Prepare(feeds []reader.Feed, active reader.Feed) Context {
+	ctx := Context{
+		Feed:    active,
+		Sidebar: make([]Navitem, len(feeds)),
+	}
+	for i, f := range feeds {
+		ctx.Sidebar[i] = Navitem{
+			Title:    f.Title(),
+			Resource: fmt.Sprintf("%d", i),
+		}
+	}
+	return ctx
+}
+
+func createHandler(feeds []reader.Feed, active reader.Feed) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("index.html")
+		if err != nil {
+			fmt.Fprintf(w, "%s", err)
+			return
+		}
+		t.Execute(w, Prepare(feeds, active))
+	}
+}
+
 func main() {
 	var port = flag.Int("port", 8080, "HTTP port")
 	flag.Parse()
@@ -37,13 +72,10 @@ func main() {
 		}
 	}()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFiles("index.html")
-		if err != nil {
-			fmt.Fprintf(w, "%s", err)
-			return
-		}
-		t.Execute(w, feeds)
-	})
+	for i, feed := range feeds {
+		http.HandleFunc(fmt.Sprintf("/%d", i), createHandler(feeds, feed))
+	}
+	http.HandleFunc("/", createHandler(feeds, nil))
+
 	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 }
